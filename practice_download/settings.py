347507025758cad7433592b7c35216b5bb1e3af1
RevHam1 <36148347+RevHam1@users.noreply.github.com>
2025-02-21 17:34:59 -0600

    Tried to deal with staticfiles

--- practice_download/settings.py
+++ practice_download/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# STATIC_URL = '/static/'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 SPOONACULAR_API_KEY = config('SPOONACULAR_API_KEY')
