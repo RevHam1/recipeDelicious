af6e072f4bc4e90f69a1f838901f0c633496d721
RevHam1 <36148347+RevHam1@users.noreply.github.com>
2025-02-21 12:13:56 -0600

    Added vervel configurations

--- practice_download/settings.py
+++ practice_download/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'iy=mhzhifm+in=a-&eas+)(x11+=l-m2qzwq$#&h7-yy6py_&d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
