63ccbc634b56b73e10b9cd1bc162f720b28b3cfa
RevHam1 <36148347+RevHam1@users.noreply.github.com>
2025-02-21 13:17:02 -0600

    Prepare for Vercel deployment

---
+++ build_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
+mkdir -p .vercel/output/static
+cp -r staticfiles/ .vercel/output/static/
+python3 manage.py makemigrations
+python3 manage.py migrate
