8af8c48d03468e2f678e062cb829d3362fddf982
RevHam1 <36148347+RevHam1@users.noreply.github.com>
2025-02-24 20:47:47 -0600

    Deleted unnecessary staticfiles folder

--- practice_download/settings.py
+++ practice_download/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'iy=mhzhifm+in=a-&eas+)(x11+=l-m2qzwq$#&h7-yy6py_&d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
